cb839c686411147548d35e23d4fefbcfd363a846
ohmstar123 <opatel22@uwo.ca>
2023-03-28 21:26:45 -0400

    Collision with Grunt in Tutorial Level
    When the player collides with the grunt, it will now bring up the fight screen

--- gameObjects.py
+++ gameObjects.py
@@ -1,20 +1,27 @@
 import pygame
 from levelSettings import *
 
-#Level 1 Sprites 
+battleLoopGrunt = False
+gruntLoopRunOnce = False
+
+# Level 1 Sprites
+
+
 class PrisionTile(pygame.sprite.Sprite):
     def __init__(self, position, groups):
         super().__init__(groups)
         self.image = pygame.image.load(
             "prisonfloortile.png").convert_alpha()
         self.rect = self.image.get_rect(topleft=position)
-        
+
+
 class DoorTile(pygame.sprite.Sprite):
     def __init__(self, position, groups):
         super().__init__(groups)
         self.image = pygame.image.load(
             "prisonDoor.png").convert_alpha()
         self.rect = self.image.get_rect(topleft=position)
+
 
 class GruntTile(pygame.sprite.Sprite):
     def __init__(self, position, groups):
@@ -23,13 +30,16 @@ class GruntTile(pygame.sprite.Sprite):
             "grunt.png").convert_alpha()
         self.rect = self.image.get_rect(topleft=position)
 
-#Level 2 Sprites
+# Level 2 Sprites
+
+
 class WaterTile(pygame.sprite.Sprite):
     def __init__(self, position, groups):
         super().__init__(groups)
         self.image = pygame.image.load(
             "watertile.png").convert_alpha()
         self.rect = self.image.get_rect(topleft=position)
+
 
 class GrassTile(pygame.sprite.Sprite):
     def __init__(self, position, groups):
@@ -38,6 +48,7 @@ class GrassTile(pygame.sprite.Sprite):
             "grasstile.png").convert_alpha()
         self.rect = self.image.get_rect(topleft=position)
 
+
 class SandTile(pygame.sprite.Sprite):
     def __init__(self, position, groups):
         super().__init__(groups)
@@ -45,11 +56,13 @@ class SandTile(pygame.sprite.Sprite):
             "sandtile.jpg").convert_alpha()
         self.rect = self.image.get_rect(topleft=position)
 
-#Level 3 Sprites
+# Level 3 Sprites
 
-#Level 4 Sprites
+# Level 4 Sprites
 
-#ALL LEVELS
+# ALL LEVELS
+
+
 class Tile(pygame.sprite.Sprite):
     def __init__(self, position, groups):
         super().__init__(groups)
@@ -57,23 +70,22 @@ class Tile(pygame.sprite.Sprite):
             "new_rock.png").convert_alpha()
         self.rect = self.image.get_rect(topleft=position)
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, position, groups, obstacle_sprites):
-        super().__init__(groups)
-        self.image = pygame.image.load("sridhar_player_icon.png").convert_alpha()
-        self.rect = self.image.get_rect(topleft=position)
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, position, groups, obstacle_sprites, grunt_sprite):
+        super().__init__(groups)
+        self.image = pygame.image.load(
+            "sridhar_player_icon.png").convert_alpha()
+        self.rect = self.image.get_rect(topleft=position)
 
         self.direction = pygame.math.Vector2()
         self.speed = 5
 
-
         self.obstacle_sprites = obstacle_sprites
-
+        self.grunt_sprite = grunt_sprite
 
     def input(self):
         keys = pygame.key.get_pressed()
-
 
         if keys[pygame.K_UP]:
             self.direction.y = -1
@@ -82,7 +94,6 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.y = 0
 
-
         if keys[pygame.K_LEFT]:
             self.direction.x = -1
         elif keys[pygame.K_RIGHT]:
@@ -90,20 +101,19 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
-
-
     def move(self):
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
-
 
         self.rect.x += self.direction.x * self.speed
         self.collision("horizontal")
         self.rect.y += self.direction.y * self.speed
         self.collision("vertical")
 
-
     def collision(self, direction):
+        global gruntLoopRunOnce
+        global battleLoopGrunt
+
         if direction == "horizontal":
             for sprite in self.obstacle_sprites:
                 if sprite.rect.colliderect(self.rect):
@@ -111,7 +121,6 @@ class Player(pygame.sprite.Sprite):
                         self.rect.right = sprite.rect.left
                     if self.direction.x < 0:
                         self.rect.left = sprite.rect.right
-
 
         if direction == "vertical":
             for sprite in self.obstacle_sprites:
@@ -121,10 +130,11 @@ class Player(pygame.sprite.Sprite):
                     if self.direction.y < 0:
                         self.rect.top = sprite.rect.bottom
 
-
-        if direction == "vertical":
-            pass
-
+        if gruntLoopRunOnce == False:
+            for sprite in self.grunt_sprite:
+                if sprite.rect.colliderect(self.rect):
+                    battleLoopGrunt = True
+                    gruntLoopRunOnce = True
 
     def update(self):
         self.input()
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import sys
 from levelSettings import *
 from level1 import Level1
 from level2 import Level2
+import gameObjects
 from debug import debug
+
 
 class Game:
     def __init__(self):
@@ -11,17 +13,13 @@ class Game:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("Super Ohio Throwdown")
         self.clock = pygame.time.Clock()
-        
+
         self.level1 = Level1()
-
-
-        
-
 
     def run(self):
         pygame.mixer.music.load("menumusic.mp3")
         battleLoopBool = False
-        
+
         pygame.mixer.music.set_volume(0)
         pygame.mixer.music.play(-1)
 
@@ -30,10 +28,14 @@ class Game:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-            
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_TAB:
                     battleLoopBool = True
+
+            if gameObjects.battleLoopGrunt == True:
+                battleLoopBool = True
+                gameObjects.battleLoopGrunt = False
 
             if battleLoopBool:
                 heroImg = pygame.image.load("sridhar_player_icon_back.png")
@@ -42,23 +44,26 @@ class Game:
                 gruntImg_rect = heroImg.get_rect()
                 battleBg = pygame.image.load("battleBgTemplate.jpg")
                 battleBg_rect = battleBg.get_rect()
-                battleScreen = pygame.display.set_mode((battleBg_rect.width, battleBg_rect.height))
+                battleScreen = pygame.display.set_mode(
+                    (battleBg_rect.width, battleBg_rect.height))
                 battleScreen_rect = battleScreen.get_rect()
                 battleRunning = True
                 battleFont = pygame.font.Font('kvn-pokemon-gen-5.ttf', 24)
 
-                optionsMenu = (1000, 600, 50, 50) 
-                
-                attack1 = (100, 535, (battleBg_rect.width - 300) / 2, 50) 
-                attack2 = ((battleBg_rect.width / 2) + 50, 535, (battleBg_rect.width - 300) / 2, 50) 
-                attack3 = (100, 630, (battleBg_rect.width - 300) / 2, 50) 
-                attack4 = ((battleBg_rect.width / 2) + 50, 630, (battleBg_rect.width - 300) / 2, 50) 
+                optionsMenu = (1000, 600, 50, 50)
+
+                attack1 = (100, 535, (battleBg_rect.width - 300) / 2, 50)
+                attack2 = ((battleBg_rect.width / 2) + 50, 535,
+                           (battleBg_rect.width - 300) / 2, 50)
+                attack3 = (100, 630, (battleBg_rect.width - 300) / 2, 50)
+                attack4 = ((battleBg_rect.width / 2) + 50, 630,
+                           (battleBg_rect.width - 300) / 2, 50)
 
                 #opponent = ((battleBg_rect.width / 2) + 158, 88, 150, 250)
                 #hero = ((battleBg_rect.width / 2) - 335, 250, 150, 250)
                 while battleRunning:
 
-                    #event loop
+                    # event loop
                     for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                             battleRunning = False
@@ -67,22 +72,24 @@ class Game:
                                 battleRunning = False
                         if event.type == pygame.MOUSEBUTTONDOWN:
                             (x, y) = pygame.mouse.get_pos()
-                            if ((x >= 100) and (x <= ((battleBg_rect.width - 300) / 2) + 100) and (y >= 535) and ( y <= 585)):
+                            if ((x >= 100) and (x <= ((battleBg_rect.width - 300) / 2) + 100) and (y >= 535) and (y <= 585)):
                                 print("attack1")
-                            elif ((x >= (battleBg_rect.width / 2) + 50) and (x <= ((battleBg_rect.width - 300) / 2) + (battleBg_rect.width / 2) + 50) and (y >= 535) and ( y <= 585)):
+                            elif ((x >= (battleBg_rect.width / 2) + 50) and (x <= ((battleBg_rect.width - 300) / 2) + (battleBg_rect.width / 2) + 50) and (y >= 535) and (y <= 585)):
                                 print("attack2")
-                            elif ((x >= 100) and (x <= ((battleBg_rect.width - 300) / 2) + 100) and (y >= 630) and ( y <= 680)):
+                            elif ((x >= 100) and (x <= ((battleBg_rect.width - 300) / 2) + 100) and (y >= 630) and (y <= 680)):
                                 print("attack3")
-                            elif ((x >= (battleBg_rect.width / 2) + 50) and (x <= ((battleBg_rect.width - 300) / 2) + (battleBg_rect.width / 2) + 50) and (y >= 630) and ( y <= 680)):
+                            elif ((x >= (battleBg_rect.width / 2) + 50) and (x <= ((battleBg_rect.width - 300) / 2) + (battleBg_rect.width / 2) + 50) and (y >= 630) and (y <= 680)):
                                 print("attack4")
                             # if optionsMenu.rect.collidepoint(event.pos):
                             #     menuText = battleFont.render('Success!', True, (0, 0, 0))
                             #     menuText_rect = menuText.get_rect()
                             #     menuText_rect.centerx = battleScreen_rect.centerx
                             #     pygame.display.update()
-                    battleScreen.blit(battleBg, (0,0))
-                    battleScreen.blit(heroImg, ((battleBg_rect.width / 2) - 335, 320))
-                    battleScreen.blit(gruntImg, ((battleBg_rect.width / 2) + 158, 88))
+                    battleScreen.blit(battleBg, (0, 0))
+                    battleScreen.blit(
+                        heroImg, ((battleBg_rect.width / 2) - 335, 320))
+                    battleScreen.blit(
+                        gruntImg, ((battleBg_rect.width / 2) + 158, 88))
 
                     pygame.draw.rect(battleScreen, (255, 0, 0), optionsMenu)
 
@@ -91,7 +98,6 @@ class Game:
                     pygame.draw.rect(battleScreen, (240, 240, 240), attack3)
                     pygame.draw.rect(battleScreen, (240, 240, 240), attack4)
 
-                    
                     #pygame.draw.rect(battleScreen, (0, 0, 0), opponent)
                     #pygame.draw.rect(battleScreen, (0, 0, 0), hero)
                     pygame.display.update()
@@ -101,8 +107,6 @@ class Game:
             self.level1.run()
             pygame.display.update()
             self.clock.tick(FPS)
-
-
 
 
 if __name__ == '__main__':
