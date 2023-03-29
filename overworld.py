763234eedab28854e839613e8818595263e68262
Tiago <talves4@uwo.ca>
2023-03-29 17:16:52 -0400

    working main menu boot to game and exit
    Authored by Tiago Alves
    to do: second screen for controls and credits, finalize textures

--- overworld.py
+++ overworld.py
@@ -1,7 +1,6 @@
-#overWorld file
-import pygame, time, random
-import gameObjects, math
-
+#main menu file
+import pygame, time
+from main import Game as game
 pygame.init()
 
 fps = 120
@@ -9,151 +8,101 @@ clock = pygame.time.Clock()
 obamaFix = 1
 
 pygame.mixer.music.load("menumusic.mp3")
-obama = pygame.mixer.Sound("letmebeclear.mp3")
-battleLoopBool = False
-test = True
 
 bg = pygame.image.load('bg2.jpg')
 bg_rect = bg.get_rect()
 keys = {'right':False, 'up':False, 'left':False, 'down':False}
 
-screen = pygame.display.set_mode((bg_rect.width, bg_rect.height))
+screen = pygame.display.set_mode((1280, 720))
 screen_rect = screen.get_rect()
 
-# creating avatar to move in overworld   
-heroImg = pygame.image.load('diamond.png')
-hero_rect = heroImg.get_rect()
-heroSpeed = 1
-'''
-hero = battleCalcs.Fighter([1, 1, 1, 1], [{
-    "Chasedown Block": battleCalcs.Move([0, 100, 10, 1, 1, 0]),
-    "Yabadabadoo Old Navy": battleCalcs.Move([0, 50, 20, 2, 0, 0]),
-    "Cleveland!! This is for You!": battleCalcs.Move([0, 100, 15, 3, 0, 0]),
-    "Tomohawk Dunk": battleCalcs.Move([100, 90, 10, 4, 0, 0])
-    }], 100)'''
+controlScreen = pygame.display.set_mode((1280, 720))
+controlScreen_rect = controlScreen.get_rect()
 
-enemy = pygame.image.load('obamacube.png')
-enemyImg = pygame.transform.scale(enemy, (100, 100))
-enemy_rect = enemy.get_rect()
-enemy_rect.x = 400
-enemy_rect.y = 300
+logo = pygame.image.load('ohiologo.png')
+logo_rect = logo.get_rect()
+logo_rect.x = (screen.get_rect().centerx - (logo_rect.width / 2))
+logo_rect.y = (screen.get_rect().centery - (logo_rect.height/2) - 150) / 2
+
+controlsTab_rect = pygame.Rect((screen.get_rect().centerx - (logo_rect.width / 2)), (screen.get_rect().centery - (logo_rect.height/2) + 50), 275, 100)
+creditsTab_rect = pygame.Rect((screen.get_rect().centerx - (logo_rect.width / 2) + 325), (screen.get_rect().centery - (logo_rect.height/2) + 50), 275, 100)
+startTab_rect = pygame.Rect((screen.get_rect().centerx - (logo_rect.width / 2)), (screen.get_rect().centery - (logo_rect.height/2) + 175), 600, 100)
+exitTab_rect = pygame.Rect((screen.get_rect().centerx - (logo_rect.width / 2)), (screen.get_rect().centery - (logo_rect.height/2) + 300), 600, 100)
+
+menuFont = pygame.font.Font('kvn-pokemon-gen-5.ttf', 48)
+controlsText = pygame.font.Font.render(menuFont, "Controls", True, (255, 255, 255))
+controlsText_rect = controlsText.get_rect()
+
+creditsText = pygame.font.Font.render(menuFont, "Credits", True, (255, 255, 255))
+creditsText_rect = controlsText.get_rect()
+
+startText = pygame.font.Font.render(menuFont, "Start", True, (255, 255, 255))
+startText_rect = controlsText.get_rect()
+print(startText_rect.width)
+
+exitText = pygame.font.Font.render(menuFont, "Exit", True, (255, 255, 255))
+exitText_rect = controlsText.get_rect()
+
+def loadingScreen():
+    screen.fill((0,0,0))
+    loadingText = pygame.font.Font.render(menuFont, "Loading...", True, (255, 255, 255))
+    loadingText_rect = loadingText.get_rect()
+    loadingText_rect.x = screen_rect.centerx
+    loadingText_rect.y = screen_rect.centery
+    screen.blit(loadingText, loadingText_rect)
+    pygame.display.update()
+    time.sleep(1)
 
 level = 0
 
-pygame.mixer.music.set_volume(0)
+pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play(-1)
 running = True
+controlsMenuActive = False
+creditsActive = False
 while running:
     #event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_TAB:
-            battleLoopBool = True
-        if event.key == pygame.K_RIGHT:
-            keys['right'] = True
-        elif event.key == pygame.K_LEFT:
-            keys['left'] = True
-        if event.key == pygame.K_UP:
-            keys['up'] = True
-        elif event.key == pygame.K_DOWN:
-            keys['down'] = True
-    if event.type == pygame.KEYUP:
-        if event.key == pygame.K_RIGHT:
-            keys['right'] = False
-        elif event.key == pygame.K_LEFT:
-            keys['left'] = False
-        if event.key == pygame.K_UP:
-            keys['up'] = False
-        elif event.key == pygame.K_DOWN:
-            keys['down'] = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            (x, y) = pygame.mouse.get_pos()
+            if (x >= startTab_rect.x and x <= (startTab_rect.x + startTab_rect.width) and y >= startTab_rect.y and y <= (startTab_rect.y + startTab_rect.height) and controlsMenuActive == False):
+                loadingScreen()
+                newGame = game()
+                newGame.__init__()
+                newGame.run()
+                print("success")
+                pygame.display.update()
+            elif (x >= exitTab_rect.x and x <= (exitTab_rect.x + exitTab_rect.width) and y >= exitTab_rect.y and y <= (exitTab_rect.y + exitTab_rect.height) and controlsMenuActive == False):
+                running = False
+            elif (x >= exitTab_rect.x and x <= (exitTab_rect.x + exitTab_rect.width) and y >= exitTab_rect.y and y <= (exitTab_rect.y + exitTab_rect.height) and controlsMenuActive == True):
+                controlsMenuActive = False
+            elif (x >= controlsTab_rect.x and x <= (controlsTab_rect.x + controlsTab_rect.width) and y >= controlsTab_rect.y and y <= (controlsTab_rect.y + controlsTab_rect.height) and controlsMenuActive == False):
+                controlsMenuActive = True
+                pygame.quit()
 
-    x, y = 0, 0
-    if keys['right'] == True and (hero_rect.x + hero_rect.width) < screen_rect.width:
-        x += heroSpeed
-    if keys['left'] == True and hero_rect.x > 0:
-        x -= heroSpeed
-    if keys['down'] == True and (hero_rect.y + hero_rect.height) < screen_rect.height:
-        y += heroSpeed
-    if keys['up'] == True and hero_rect.y > 0:
-        y -= heroSpeed
-    
-    if x != 0 and y != 0:
-        if x > 0 and y > 0:
-            x = math.sqrt(x)
-            y = math.sqrt(y)
-        elif x > 0 and y < 0:
-            x = math.sqrt(x)
-            y = -y
-            y = math.sqrt(y)
-            y = -y
-        elif x < 0 and y > 0:
-            x = -x
-            x = math.sqrt(x)
-            x = -x
-            y = math.sqrt(y)
-        else:
-            x = -x
-            x = math.sqrt(x)
-            x = -x
-            y = -y
-            y = math.sqrt(y)
-            y = -y
 
-    hero_rect.x += x
-    hero_rect.y += y
-
-    '''if hero_rect.colliderect(enemy_rect):
-        enemy_rect.x = 0
-        #pygame.mixer.music.pause()
-        #pygame.mixer.Sound.play(obama)
-        #time.sleep(2.50)
-        #enemyImg.set_alpha(0)
-        #pygame.mixer.music.unpause()
-        screen.fill((0, 0, 0))
-        battleLoopBool = True'''
-
-    #battle loop
-    if battleLoopBool:
-        print("placeholder")
-        battleBg = pygame.image.load("battleBgTemplate.jpg")
-        battleBg_rect = battleBg.get_rect()
-        battleScreen = pygame.display.set_mode((battleBg_rect.width, battleBg_rect.height))
-        battleScreen_rect = battleScreen.get_rect()
-        battleRunning = True
-        battleFont = pygame.font.Font('kvn-pokemon-gen-5.ttf', 24)
-
-        optionsMenu = (1000, 600, 50, 50) 
-        while battleRunning:
-
-            #event loop
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    battleRunning = False
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_BACKSPACE:
-                        battleRunning = False
-                        print ("test")
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if optionsMenu.rect.collidepoint(event.pos):
-                        menuText = battleFont.render('Success!', True, (0, 0, 0))
-                        menuText_rect = menuText.get_rect()
-                        menuText_rect.centerx = battleScreen_rect.centerx
-                        pygame.display.update()
-            battleScreen.blit(battleBg, (0,0))
-            pygame.draw.rect(battleScreen, (255, 0, 0), optionsMenu)
+    while controlsMenuActive:
+            controlScreen.blit(bg, (0,0))
+            controlsMenuText = pygame.font.Font.render(menuFont, "WASD to move", True, (255, 255, 255))
+            controlScreen.blit(controlsMenuText, ((controlsTab_rect.centerx - 90), (controlsTab_rect.centery - 50)))
+            pygame.draw.rect(controlScreen, (0, 0, 255), exitTab_rect)
+            controlScreen.blit(exitText, ((exitTab_rect.centerx), (exitTab_rect.centery - 50)))
             pygame.display.update()
-        battleLoopBool = False
-        
-        
+            clock.tick(fps)
 
-    if (test):
-        screen.blit(bg, (0,0))
-        screen.blit(enemyImg, enemy_rect)
-        screen.blit(heroImg, hero_rect)
-        pygame.display.flip()
-        clock.tick(fps)
+    screen.blit(bg, (0,0))
+    screen.blit(logo, logo_rect)
+    pygame.draw.rect(screen, (255, 0, 0), controlsTab_rect)
+    screen.blit(controlsText, ((controlsTab_rect.centerx - 90), (controlsTab_rect.centery - 50)))
+    pygame.draw.rect(screen, (255, 0, 0), creditsTab_rect)
+    screen.blit(creditsText, ((creditsTab_rect.centerx - 75), (creditsTab_rect.centery - 50)))
+    pygame.draw.rect(screen, (0, 255, 0), startTab_rect)
+    screen.blit(startText, ((startTab_rect.centerx - (startText_rect.width/2)), (startTab_rect.centery - 50)))
+    pygame.draw.rect(screen, (0, 0, 255), exitTab_rect)
+    screen.blit(exitText, ((exitTab_rect.centerx), (exitTab_rect.centery - 50)))
+    pygame.display.flip()
+    clock.tick(fps)
 
 pygame.quit()
