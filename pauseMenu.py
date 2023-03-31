54a677137bf7ad18abcba1ad59c7a55c04a8841e
apate695 <apate695@uwo.ca>
2023-03-31 17:32:58 -0400

    fix scaling for pause menu

--- pauseMenu.py
+++ pauseMenu.py
@@ -21,10 +21,10 @@ def p():
         move_3=f.render(main.hero.move3name, True,(0,0,0))
         move_4=f.render(main.hero.move4name, True,(0,0,0))
         h_r=heroImg.get_rect(center=(WIDTH/2-300,HEIGHT/2-100))
-        move_1_r=move_1.get_rect(center=(WIDTH/2+300,HEIGHT/2-100))
-        move_2_r=move_2.get_rect(center=(WIDTH/2+300,HEIGHT/2-0))
-        move_3_r=move_3.get_rect(center=(WIDTH/2+300,HEIGHT/2+100))
-        move_4_r=move_4.get_rect(center=(WIDTH/2+300,HEIGHT/2+200))
+        move_1_r=move_1.get_rect(center=(WIDTH/2+300,HEIGHT/2-200))
+        move_2_r=move_2.get_rect(center=(WIDTH/2+300,HEIGHT/2-150))
+        move_3_r=move_3.get_rect(center=(WIDTH/2+300,HEIGHT/2-100))
+        move_4_r=move_4.get_rect(center=(WIDTH/2+300,HEIGHT/2-50))
 
         continue_r= continue_t.get_rect(center=(WIDTH/2-300,HEIGHT*9/10))
         quit_r= quit_t.get_rect(center=(WIDTH/2+300,HEIGHT*9/10))
@@ -42,13 +42,11 @@ def p():
         pygame.draw.rect(screen,(0,0,0), move_2_r,2)
         pygame.draw.rect(screen,(0,0,0), move_3_r,2)
         pygame.draw.rect(screen,(0,0,0), move_4_r,2)
-        pygame.draw.rect(screen,(0,0,0), h_r,2)
+        pygame.draw.rect(screen,(0,0,0), h_r)
 
-        box = pygame.Rect(WIDTH/2-225, HEIGHT/2-200, 350, 460)
-        pygame.draw.rect(screen, (255, 255, 255), box)
-        pygame.draw.rect(screen, (0, 0, 0), box, 2)
+        
 
-        descBox = pygame.Rect(WIDTH/2-350, HEIGHT/2+230, 350, 60)
+        descBox = pygame.Rect(WIDTH/2-175, HEIGHT/2+200, 350, 60)
         pygame.draw.rect(screen, (255, 255, 255), descBox)
         pygame.draw.rect(screen, (0, 0, 0), descBox, 2)
 
@@ -59,28 +57,29 @@ def p():
         screen.blit(move_2,move_2_r)
         screen.blit(move_3,move_3_r)
         screen.blit(move_4,move_4_r)
-        f=pygame.font.Font(None,20)
-        lvl_box = pygame.Rect(WIDTH/2-225, HEIGHT/2-460, 350, 30)
+        f=pygame.font.Font(None,30)
+        lvl_box = pygame.Rect(WIDTH/2-460, HEIGHT/2-350, 320, 30)
         pygame.draw.rect(screen, (255, 255, 255), lvl_box)
         pygame.draw.rect(screen, (0, 0, 0), lvl_box, 2)
 
-        stat_box = pygame.Rect(WIDTH/2-225, HEIGHT/2+50, 350, 50)
+        stat_box = pygame.Rect(WIDTH/2-460, HEIGHT/2+100, 320, 50)
         pygame.draw.rect(screen, (255, 255, 255), stat_box)
         pygame.draw.rect(screen, (0, 0, 0), stat_box, 2)
         lvl_text = f"LVL: {player_stats['lvl']}"
         lvl_render = f.render(lvl_text, True, (0, 0, 0))
 
-        lvl_render_rect = lvl_render.get_rect(center=(WIDTH/2-150,HEIGHT/2-217))
+        lvl_render_rect = lvl_render.get_rect(center=(WIDTH/2-380,HEIGHT/2-335))
         screen.blit(lvl_render, lvl_render_rect)
+        f=pygame.font.Font(None,20)
 
         stat_text = f"HP: {player_stats['hp']}  ATK: {player_stats['atk']}"
         stat_text_2=f"DEF: {player_stats['def']}  SPD: {player_stats['spd']}"
         stat_render = f.render(stat_text, True, (0, 0, 0))
-        stat_render_rect = stat_render.get_rect(center=(WIDTH/2-150,HEIGHT/2+60))
+        stat_render_rect = stat_render.get_rect(center=(WIDTH/2-350,HEIGHT/2+120))
         screen.blit(stat_render, stat_render_rect)
 
         stat_render_2 = f.render(stat_text_2, True, (0, 0, 0))
-        stat_render_rect_2 = stat_render_2.get_rect(center=(WIDTH/2-150,HEIGHT/2+80))
+        stat_render_rect_2 = stat_render_2.get_rect(center=(WIDTH/2-350,HEIGHT/2+140))
         screen.blit(stat_render_2, stat_render_rect_2)
 
         pygame.display.update()
@@ -96,26 +95,28 @@ def p():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     paused = not paused
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            elif event.type == pygame.MOUSEBUTTONDOWN:
                 if paused:
                     f=pygame.font.Font(None,50)
                     continue_t=f.render('Continue', True, (0,0,0),2)
                     quit_t=f.render('Quit',True,(0,0,0),2)
-                    continue_r= continue_t.get_rect(center=(WIDTH/2-150,HEIGHT*9/10))
-                    quit_r= quit_t.get_rect(center=(WIDTH/2+150,HEIGHT*9/10))
+                    continue_r= continue_t.get_rect(center=(WIDTH/2-300,HEIGHT*9/10))
+                    quit_r= quit_t.get_rect(center=(WIDTH/2+300,HEIGHT*9/10))
                     move_1=f.render(main.hero.move1name, True,(0,0,0))
                     move_2=f.render(main.hero.move2name, True,(0,0,0))
                     move_3=f.render(main.hero.move3name, True,(0,0,0))
                     move_4=f.render(main.hero.move4name, True,(0,0,0))
-                    move_1_r=move_1.get_rect(center=(WIDTH/2+150,HEIGHT/2-100))
-                    move_2_r=move_2.get_rect(center=(WIDTH/2+150,HEIGHT/2-50))
-                    move_3_r=move_3.get_rect(center=(WIDTH/2+150,HEIGHT/2))
-                    move_4_r=move_4.get_rect(center=(WIDTH/2+150,HEIGHT/2+50))
+                    move_1_r=move_1.get_rect(center=(WIDTH/2+150,HEIGHT/2-200))
+                    move_2_r=move_2.get_rect(center=(WIDTH/2+150,HEIGHT/2-150))
+                    move_3_r=move_3.get_rect(center=(WIDTH/2+150,HEIGHT/2-100))
+                    move_4_r=move_4.get_rect(center=(WIDTH/2+150,HEIGHT/2-50))
                     mouse_pos = pygame.mouse.get_pos()
                     if continue_r.collidepoint(mouse_pos):
                         paused = False
+                        return False
                     elif quit_r.collidepoint(mouse_pos):
                         running = False
+                        return True
                     elif move_1_r.collidepoint(mouse_pos):
                         print()
                     elif move_2_r.collidepoint(mouse_pos):
