295d078148eb815a5c1c32e53b89c8449fdc92f1
apate695 <apate695@uwo.ca>
2023-03-31 20:08:00 -0400

    changed font

--- pauseMenu.py
+++ pauseMenu.py
@@ -12,7 +12,7 @@ def p():
     
 
     def pause():
-        f=pygame.font.Font(None,50)
+        f=pygame.font.Font('kvn-pokemon-gen-5.ttf',50)
         continue_t=f.render('Continue', True, (0,0,0))
         quit_t=f.render('Quit',True,(0,0,0))
         
@@ -57,7 +57,7 @@ def p():
         screen.blit(move_2,move_2_r)
         screen.blit(move_3,move_3_r)
         screen.blit(move_4,move_4_r)
-        f=pygame.font.Font(None,30)
+        f=pygame.font.Font('kvn-pokemon-gen-5.ttf',30)
         lvl_box = pygame.Rect(WIDTH/2-460, HEIGHT/2-350, 320, 30)
         pygame.draw.rect(screen, (255, 255, 255), lvl_box)
         pygame.draw.rect(screen, (0, 0, 0), lvl_box, 2)
@@ -70,7 +70,7 @@ def p():
 
         lvl_render_rect = lvl_render.get_rect(center=(WIDTH/2-380,HEIGHT/2-335))
         screen.blit(lvl_render, lvl_render_rect)
-        f=pygame.font.Font(None,20)
+        f=pygame.font.Font('kvn-pokemon-gen-5.ttf',20)
 
         stat_text = f"HP: {player_stats['hp']}  ATK: {player_stats['atk']}"
         stat_text_2=f"DEF: {player_stats['def']}  SPD: {player_stats['spd']}"
